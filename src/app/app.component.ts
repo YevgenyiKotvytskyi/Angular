f9dd8dae2dd7509ae9da953be867a1352fde1dca
Yevgenyi Kotvytskyi <yegeniy.kotvytskyi@gmail.com>
2020-08-29 13:39:24 +0300

    List with icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
+export interface Section {
+  name: string;
+  updated: Date;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'users-list';
+  typesOfShoes: string[] = ['Boots', 'Clogs', 'Loafers', 'Moccasins', 'Sneakers'];
+  notes:Section[]  = [
+    {
+      name: 'Vacation Itinerary',
+      updated: new Date('2/20/20'),
+    },
+    {
+      name: 'Kitchen Remodel',
+      updated: new Date('1/18/20'),
+    }
+  ];
 }
